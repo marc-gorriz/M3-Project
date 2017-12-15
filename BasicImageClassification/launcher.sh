3a0b1314527cdb7f14a1aa9f67bde9c7facc139a
Marc Górriz <gbmarc@hotmail.es>
2017-12-15 19:54:59 +0100

    new changes

--- BasicImageClassification/launcher.sh
+++ BasicImageClassification/launcher.sh
@@ -9,10 +9,10 @@ K_FOLD_K=5
 
 if [ "$1" = "train" ]
 then
-    srun --pty --mem 30G python3 main.py --data_path $DATA_PATH --output_path MODEL_PATH --classifier CLASSIFIER --train_method TRAIN_METHOD --kfold_k K_FOLD_K --train
+    srun --pty --mem 30G python3 -i main.py --data_path $DATA_PATH --output_path MODEL_PATH --classifier CLASSIFIER --train_method TRAIN_METHOD --kfold_k K_FOLD_K --train
 elif [ "$1" = "test" ]
 then
-    srun --pty --mem 30G python3 main.py --data_path $DATA_PATH --output_path MODEL_PATH --classifier CLASSIFIER --train_method TRAIN_METHOD --kfold_k K_FOLD_K --test
+    srun --pty --mem 30G python3 -i main.py --data_path $DATA_PATH --output_path MODEL_PATH --classifier CLASSIFIER --train_method TRAIN_METHOD --kfold_k K_FOLD_K --test
 else
     echo "Incorrect option"
 fi
