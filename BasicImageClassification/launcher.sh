d06919af4bca9137caf9b275431da52307bafa31
Marc Górriz <gbmarc@hotmail.es>
2017-12-15 19:50:17 +0100

    new changes

--- BasicImageClassification/launcher.sh
+++ BasicImageClassification/launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DATA_PATH="data_path"
-MODEL_PATH="model_path"
+DATA_PATH="/imatge/mgorriz/work/master/Databases/MIT_split"
+MODEL_PATH="/imatge/mgorrizwork/master/models/session01"
 CLASSIFIER="knn"
 TRAIN_METHOD="kfold"
 K_FOLD_K=5
@@ -9,10 +9,10 @@ K_FOLD_K=5
 
 if [ "$1" = "train" ]
 then
-    python3 main.py --data_path $DATA_PATH --output_path MODEL_PATH --classifier CLASSIFIER --train_method TRAIN_METHOD --kfold_k K_FOLD_K --train
+    srun --pty --mem 30G python3 main.py --data_path $DATA_PATH --output_path MODEL_PATH --classifier CLASSIFIER --train_method TRAIN_METHOD --kfold_k K_FOLD_K --train
 elif [ "$1" = "test" ]
 then
-    python3 main.py --data_path $DATA_PATH --output_path MODEL_PATH --classifier CLASSIFIER --train_method TRAIN_METHOD --kfold_k K_FOLD_K --test
+    srun --pty --mem 30G python3 main.py --data_path $DATA_PATH --output_path MODEL_PATH --classifier CLASSIFIER --train_method TRAIN_METHOD --kfold_k K_FOLD_K --test
 else
     echo "Incorrect option"
 fi
