bfd258796d922f707008b8253d3db8b3834dae29
Marc Górriz <gbmarc@hotmail.es>
2017-12-16 13:33:18 +0100

    new changes

--- BasicImageClassification/launcher.sh
+++ BasicImageClassification/launcher.sh
@@ -8,10 +8,10 @@ TRAIN_METHOD="fixed"
 
 if [ "$1" = "train" ]
 then
-    srun --pty --mem 30G python2 main.py -i  --data_path $DATA_PATH --model_path $MODEL_PATH --evaluation_path $EVALUATION_PATH --classifier $CLASSIFIER --train_method $TRAIN_METHOD --train
+    srun --pty --mem 30G python2 -i main.py --data_path $DATA_PATH --model_path $MODEL_PATH --evaluation_path $EVALUATION_PATH --classifier $CLASSIFIER --train_method $TRAIN_METHOD --train
 elif [ "$1" = "test" ]
 then
-    srun --pty --mem 30G python2 main.py -i  --data_path $DATA_PATH --model_path $MODEL_PATH --evaluation_path $EVALUATION_PATH --classifier $CLASSIFIER --train_method $TRAIN_METHOD --test
+    srun --pty --mem 30G python2 -i main.py  --data_path $DATA_PATH --model_path $MODEL_PATH --evaluation_path $EVALUATION_PATH --classifier $CLASSIFIER --train_method $TRAIN_METHOD --test
 else
     echo "Incorrect option"
 fi
