b468e1824a575bfc948c7a42ef928c42a3e9bdeb
Marc Górriz <gbmarc@hotmail.es>
2017-12-17 00:31:19 +0100

    new changes

--- BasicImageClassification/launcher.sh
+++ BasicImageClassification/launcher.sh
@@ -9,10 +9,10 @@ TRAIN_METHOD="kfold"
 
 if [ "$1" = "train" ]
 then
-    srun --pty -c 2 --mem 30G python2 main.py --data_path $DATA_PATH --model_path $MODEL_PATH --evaluation_path $EVALUATION_PATH --descriptor $FEATURES_DESCRIPTOR --classifier $CLASSIFIER --train_method $TRAIN_METHOD --train
+    srun --pty -c 8 --mem 30G python2 main.py --data_path $DATA_PATH --model_path $MODEL_PATH --evaluation_path $EVALUATION_PATH --descriptor $FEATURES_DESCRIPTOR --classifier $CLASSIFIER --train_method $TRAIN_METHOD --train
 elif [ "$1" = "test" ]
 then
-    srun --pty -c 2 --gres=gpu:1,gmem:12G --mem 30G python2 main.py  --data_path $DATA_PATH --model_path $MODEL_PATH --evaluation_path $EVALUATION_PATH --descriptor $FEATURES_DESCRIPTOR --classifier $CLASSIFIER --train_method $TRAIN_METHOD --test
+    srun --pty -c 8 --gres=gpu:1,gmem:12G --mem 30G python2 main.py  --data_path $DATA_PATH --model_path $MODEL_PATH --evaluation_path $EVALUATION_PATH --descriptor $FEATURES_DESCRIPTOR --classifier $CLASSIFIER --train_method $TRAIN_METHOD --test
 else
     echo "Incorrect option"
 fi
