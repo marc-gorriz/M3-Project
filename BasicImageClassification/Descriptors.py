e851779badb05db3cb887edd395d60f92dc0492c
Marc Górriz <gbmarc@hotmail.es>
2017-12-17 00:04:26 +0100

    new changes

---
+++ BasicImageClassification/Descriptors.py
@@ -0,0 +1,104 @@
+import time
+
+import cv2
+import numpy as np
+
+
+class SIFT:
+    def __init__(self, nfeatures):
+        """
+
+        :param nfeatures:
+        """
+        self.nfeatures = nfeatures
+
+        # TODO: detect if python2 or python3 is used
+        # create the SIFT detector object
+        self.SIFTdetector = cv2.xfeatures2d.SIFT_create(nfeatures=self.nfeatures)  # @python3
+        # self.SIFTdetector = cv2.SIFT(nfeatures=self.nfeatures) @python2
+
+    def image_features(self, ima):
+        """
+
+        :param ima:
+        :return:
+        """
+
+        gray = cv2.cvtColor(ima, cv2.COLOR_BGR2GRAY)
+        kpt, des = self.SIFTdetector.detectAndCompute(gray, None)
+        return kpt, des
+
+    def extract_features(self, data_dictionary):
+        """
+
+        :param data:
+        :return:
+        """
+        start_time = time.time()
+
+        Train_descriptors = []
+
+        for idx in range(len(data_dictionary['filenames'])):
+            ima = cv2.imread(data_dictionary['filenames'][idx])
+            kpt, des = self.image_features(ima)
+            Train_descriptors.append(des)
+
+        data = Train_descriptors[0]
+        labels = np.array([data_dictionary['labels'][0]] * Train_descriptors[0].shape[0])
+
+        for idx in range(1, len(Train_descriptors)):
+            data = np.vstack((data, Train_descriptors[idx]))
+            labels = np.hstack((labels, np.array([data_dictionary['labels'][idx]] * Train_descriptors[idx].shape[0])))
+
+        print('SIFT features extracted: done in ' + str(time.time() - start_time) + ' secs')
+
+        return data, labels
+
+class SURF:
+    def __init__(self, nOctaves=4, nOctaveLayers=2):
+        """
+
+        :param nfeatures:
+        """
+        self.nOctaves=nOctaves
+        self.nOctaveLayers=nOctaveLayers
+
+
+        self.SURFdetector = cv2.xfeatures2d.SURF_create(nOctaves=self.nOctaves, nOctaveLayers=self.nOctaveLayers)
+
+    def image_features(self, ima):
+        """
+
+        :param ima:
+        :return:
+        """
+
+        gray = cv2.cvtColor(ima, cv2.COLOR_BGR2GRAY)
+        kpt, des = self.SURFdetector.detectAndCompute(gray, None)
+        return kpt, des
+
+    def extract_features(self, data_dictionary):
+        """
+
+        :param data:
+        :return:
+        """
+        start_time = time.time()
+
+        Train_descriptors = []
+
+        for idx in range(len(data_dictionary['filenames'])):
+            ima = cv2.imread(data_dictionary['filenames'][idx])
+            kpt, des = self.image_features(ima)
+            Train_descriptors.append(des)
+
+        data = Train_descriptors[0]
+        labels = np.array([data_dictionary['labels'][0]] * Train_descriptors[0].shape[0])
+
+        for idx in range(1, len(Train_descriptors)):
+            data = np.vstack((data, Train_descriptors[idx]))
+            labels = np.hstack((labels, np.array([data_dictionary['labels'][idx]] * Train_descriptors[idx].shape[0])))
+
+        print('SIFT features extracted: done in ' + str(time.time() - start_time) + ' secs')
+
+        return data, labels
--- BasicImageClassification/KNN.py
+++ BasicImageClassification/KNN.py
@@ -5,37 +5,28 @@ import cv2
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 
-from SIFT import SIFT
+from Descriptors import SIFT, SURF
 
 
 class KNN:
-    def __init__(self, nneighbors, njobs=-1):
+    def __init__(self, nneighbors, descriptor='SIFT', njobs=-1):
         """
 
         :param nneighbors:
         :param njobs:
         """
         self.nneighbors = nneighbors
+        self.descriptor = descriptor
         self.njobs = njobs
 
-    def process_train_data(self, data_dictionary):
-        """
+        if self.descriptor == 'SIFT':
+            self.mySIFT = SIFT(nfeatures=100)
 
-        :param data:
-        :return:
-        """
-        # TODO: extend to other descriptors, put outside and pass as a parameter.
-        mySIFT = SIFT(nfeatures=100)
-        train_features = mySIFT.extract_features(data_dictionary)
+        elif self.descriptor == 'SURF':
+            self.mySURF = SURF()
 
-        data = train_features[0]
-        labels = np.array([data_dictionary['labels'][0]] * train_features[0].shape[0])
-
-        for idx in range(1, len(train_features)):
-            data = np.vstack((data, train_features[idx]))
-            labels = np.hstack((labels, np.array([data_dictionary['labels'][idx]] * train_features[idx].shape[0])))
-
-        return data, labels
+        else:
+            print('Invalid descriptor')
 
     def train(self, data_dictionary):
         """
@@ -46,7 +37,7 @@ class KNN:
         :return:
         """
 
-        data, labels = self.process_train_data(data_dictionary)
+        data, labels = self.mySIFT.extract_features(data_dictionary)
 
         model = KNeighborsClassifier(n_neighbors=self.nneighbors, n_jobs=self.njobs)
         model.fit(data, labels)
@@ -61,8 +52,6 @@ class KNN:
         :param model_name:
         :return:
         """
-        # TODO: same commentary
-        mySIFT = SIFT(nfeatures=100)
         nFiles = len(data)
         predictions = np.array([])
 
@@ -74,7 +63,7 @@ class KNN:
 
             filename = data[i]
             ima = cv2.imread(filename)
-            _, des = mySIFT.image_features(ima)
+            _, des = self.mySIFT.image_features(ima)
 
             prediction = model.predict(des)
             values, counts = np.unique(prediction, return_counts=True)
