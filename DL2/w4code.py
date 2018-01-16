cf7192f742879a34f39f27ca677c6ef6053817fa
Marc Górriz <gbmarc@hotmail.es>
2018-01-16 19:41:09 +0100

    DL2

--- DL2/w4code.py
+++ DL2/w4code.py
@@ -42,7 +42,7 @@ def preprocess_input(x, dim_ordering='default'):
 # create the base pre-trained model
 base_model = VGG16(weights='imagenet')
 
-x = base_model.get_layer('block3_pool').output
+x = base_model.get_layer('block4_pool').output
 #x = Conv2D(64,1, activation='relu')(x)
 x = Flatten(name='flatten')(x)
 x = Dense(1024, activation='relu', name='fc1')(x)
@@ -61,6 +61,7 @@ for layer in model.layers:
 model.summary()
 
 #preprocessing_function=preprocess_input,
+"""
 datagen = ImageDataGenerator(featurewise_center=False,
     samplewise_center=False,
     featurewise_std_normalization=False,
@@ -77,6 +78,9 @@ datagen = ImageDataGenerator(featurewise_center=False,
     horizontal_flip=False,
     vertical_flip=False,
     rescale=None)
+"""
+#train_datagen = ImageDataGenerator(rescale=1. / 255, horizontal_flip=True)
+train_datagen = ImageDataGenerator()
 
 train_generator = datagen.flow_from_directory(train_data_dir,
         target_size=(img_width, img_height),
