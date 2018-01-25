34fbfe6a75c1d676fabebbd32694a8c73c519250
Fina <Fina@iMac-de-Fina.local>
2018-01-25 18:59:10 +0100

    DL3

--- DL3/data_split.py
+++ DL3/data_split.py
@@ -19,15 +19,13 @@ train_data_dir = '../../Databases/MIT_split/train/'
 test_data_dir = '../../Databases/MIT_split/test/'
 
 output_train_data_dir = '../../Databases/MIT_split_validation/train/'
-output_validation_data_dir = '../../Databases/MIT_split_validation/train/'
-output_test_data_dir = '../../Databases/MIT_split_validation/train/'
+output_validation_data_dir = '../../Databases/MIT_split_validation/validation/'
+output_test_data_dir = '../../Databases/MIT_split_validation/test/'
 
 if not os.path.exists(output_train_data_dir):
-    os.makedirs(output_train_data_dir)
-    
+    os.makedirs(output_train_data_dir)   
 if not os.path.exists(output_validation_data_dir):
     os.makedirs(output_validation_data_dir)
-    
 if not os.path.exists(output_test_data_dir):
     os.makedirs(output_test_data_dir)
 
@@ -42,20 +40,29 @@ output_validation = []
 output_test = []
 
 for c in classes:
+	
+	train_path = output_train_data_dir + c + "/"
+	validation_path = output_validation_data_dir + c + "/"
+	test_path = output_test_data_dir + c + "/"
+	
+	if not os.path.exists(train_path):
+    	os.makedirs(train_path)
+    if not os.path.exists(validation_path):
+    	os.makedirs(validation_path)
+    if not os.path.exists(test_path):
+    	os.makedirs(test_path)
+
 	train_validation_names, train_names = path_list(train_data_dir + c, nb_train_class)
 	test_validation_names, test_names = path_list(test_data_dir + c, nb_test_class)
 	
 	for name in train_validation_names:
-		shutil.copy(name, output_validation_data_dir)
-		
+		shutil.copy(name, validation_path)
 	for name in train_names:
-		shutil.copy(name, output_train_data_dir)
-		
+		shutil.copy(name, train_path)
 	for name in test_validation_names:
-		shutil.copy(name, output_validation_data_dir)
-		
+		shutil.copy(name, validation_path)
 	for name in test_names:
-		shutil.copy(name, output_test_data_dir)
+		shutil.copy(name, test_path)
 
 
 
